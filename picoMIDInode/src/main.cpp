83f036d8547634ca40a205fbb5153e13cef7331a
Edwin Dertien <e.dertien@utwente.nl>
2025-01-01 19:15:03 +0100

    sources for MIDI pico device added

---
+++ picoMIDInode/src/main.cpp
@@ -0,0 +1,442 @@
+/******************************************************************
+ * This source uses the picoServo board and RPI PICO W
+ * it functions as USB-MIDI device, using 
+ * I2C sensors / M5 potentiometer array and OLED for feedback
+ * 
+ * 
+ */
+
+
+/*********************************************************************
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ Copyright (c) 2019 Ha Thach for Adafruit Industries
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+
+/* This sketch is enumerated as USB MIDI device. 
+ * Following library is required
+ * - MIDI Library by Forty Seven Effects
+ *   https://github.com/FortySevenEffects/arduino_midi_library
+ */
+//    FILE: M5ANGLE8_analogRead8.ino
+//  AUTHOR: Rob Tillaart
+// PURPOSE: demo (works well with build in plotter)
+//     URL: https://github.com/RobTillaart/M5ANGLE8
+/*********************************************************************
+ MIT license, check LICENSE for more information
+ Copyright (c) 2023 rppicomidi
+ Modified from device_info.ino from 
+ https://github.com/sekigon-gonnoc/Pico-PIO-USB/blob/main/examples/arduino/device_info/device_info.ino
+ 2nd Copyright notice below included per license terms.
+*********************************************************************/
+
+/*********************************************************************
+ Adafruit invests time and resources providing this open source code,
+ please support Adafruit and open-source hardware by purchasing
+ products from Adafruit!
+
+ MIT license, check LICENSE for more information
+ Copyright (c) 2019 Ha Thach for Adafruit Industries
+ All text above, and the splash screen below must be included in
+ any redistribution
+*********************************************************************/
+#include <Arduino.h>
+#include <MIDI.h>
+#include "Adafruit_TinyUSB.h"
+// USB MIDI object
+Adafruit_USBD_MIDI usb_midi;
+// Create a new instance of the Arduino MIDI Library,
+// and attach usb_midi as the transport.
+MIDI_CREATE_INSTANCE(Adafruit_USBD_MIDI, usb_midi, MIDI);
+/////////// for the M5 angle sensor on I2C port 0 //////////////////
+#include "m5angle8.h"
+M5ANGLE8 MM;
+/////////// for the SSD1306 OLED DISPLAY on I2C port 0 //////////////////
+#include <U8g2lib.h>
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+//U8G2_SSD1306_128X32_UNIVISION_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); 
+/////////// for the BNO055 orientation sensor on I2C port 0 //////////////////
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
+/////////// function prototypes //////////////////
+void handleNoteOn(byte channel, byte pitch, byte velocity);
+void handleNoteOff(byte channel, byte pitch, byte velocity);
+void processEvent(sensors_event_t* event, sensors_event_t* accel);
+void displayUpdate();
+
+#include "pio_usb.h"
+// Add USB MIDI Host support to Adafruit_TinyUSB
+#include "usb_midi_host.h"
+// USB Host object
+Adafruit_USBH_Host USBHost;
+// holding device descriptor
+tusb_desc_device_t desc_device;
+// holding the device address of the MIDI device
+uint8_t midi_dev_addr = 0;
+// Pin D+ for host, D- = D+ + 1
+#ifndef PIN_USB_HOST_DP
+#define PIN_USB_HOST_DP  6
+#endif
+#define LANGUAGE_ID 0x0409  // English
+
+void tuh_mount_cb (uint8_t daddr);
+void tuh_umount_cb(uint8_t daddr);
+void print_device_descriptor(tuh_xfer_t* xfer);
+static void _convert_utf16le_to_utf8(const uint16_t *utf16, size_t utf16_len, uint8_t *utf8, size_t utf8_len);
+static int _count_utf8_bytes(const uint16_t *buf, size_t len);
+static void print_utf16(uint16_t *temp_buf, size_t buf_len);
+void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx);
+void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance);
+void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets);
+void tuh_midi_tx_cb(uint8_t dev_addr);
+
+
+
+
+void setup() {
+  if (!TinyUSBDevice.isInitialized()) {
+    TinyUSBDevice.begin(0);
+  }
+  //Serial.begin(115200);  // not useful when using USB MIDI - it is not a Teensy :)
+  pinMode(LED_BUILTIN, OUTPUT);
+  // THE FOLLOWING LINE CAUSES THE SYSTEM TO NOT RUN: 
+  // usb_midi.setStringDescriptor("TinyUSB MIDI");
+
+  // Initialize MIDI, and listen to all MIDI channels
+  // This will also call usb_midi's begin()
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+
+  // If already enumerated, additional class driverr begin() e.g msc, hid, midi won't take effect until re-enumeration
+  if (TinyUSBDevice.mounted()) {
+    TinyUSBDevice.detach();
+    delay(10);
+    TinyUSBDevice.attach();
+  }
+
+  // Attach the handleNoteOn function to the MIDI Library. It will
+  // be called whenever the Bluefruit receives MIDI Note On messages.
+  MIDI.setHandleNoteOn(handleNoteOn);
+
+  // Do the same for MIDI Note Off messages.
+  MIDI.setHandleNoteOff(handleNoteOff);
+ // Wire.setSDA(4);  // not necessary as these are the defaults... 
+ // Wire.setSCL(5);
+  Wire.begin();
+  MM.begin();
+  u8g2.begin();
+  displayUpdate();
+  if (!bno.begin())
+  {
+    /* There was a problem detecting the BNO055 ... check your connections */
+    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    while (1);
+  }
+}
+volatile int value[12];  // used to output AND print
+
+void loop() {
+  static unsigned long looptime;
+  static unsigned long displaytime;
+  sensors_event_t orientationData, accelerometerData;
+  static int oldvalue[12];
+#ifdef TINYUSB_NEED_POLLING_TASK
+  // Manual call tud_task since it isn't called by Core's background
+  TinyUSBDevice.task();
+#endif
+
+  // not enumerated()/mounted() yet: nothing to do
+  if (!TinyUSBDevice.mounted()) {
+    return;
+  }
+  if (millis() > looptime + 9) {
+    looptime = millis();
+    bno.getEvent(&orientationData, Adafruit_BNO055::VECTOR_EULER);
+    bno.getEvent(&accelerometerData, Adafruit_BNO055::VECTOR_ACCELEROMETER);
+    processEvent(&orientationData, &accelerometerData );
+    for (int i = 0; i < 8; i++) {
+      value[i] = MM.analogRead(i, 8) / 2;
+      delayMicroseconds(100);
+      if (value[i] != oldvalue[i]) {
+        MIDI.sendControlChange(i, value[i], 1);
+      }
+
+      oldvalue[i] = value[i];
+    }
+    if (millis() > displaytime + 99) {
+      displaytime = millis();
+      displayUpdate();
+    }
+  }
+  // read any new MIDI messages
+  MIDI.read();
+}
+void processEvent(sensors_event_t* event, sensors_event_t* accel){
+      value[8]=map(accel->acceleration.x,-20,20,0,127);
+      value[9]=map(accel->acceleration.y,-20,20,0,127);
+      value[10]=map(event->orientation.x,0,360,0,127);
+}
+void displayUpdate() {
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_6x10_tf);
+  u8g2.setFontRefHeightExtendedText();
+  u8g2.setDrawColor(1);
+  u8g2.setFontPosTop();
+  u8g2.setFontDirection(0);
+  u8g2.drawStr(0, 0, "mini MIDI");
+  for (int n = 0; n < 12; n++) {
+    //oled.fillRect(n * 8, 16 , 4, 32, SSD1306_INVERSE);
+    u8g2.drawBox(n * 5, 32 - (value[n] / 8), 4, (value[n] / 8));
+  }
+  u8g2.sendBuffer();
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+  // Log when a note is pressed.
+  Serial.print("Note on: channel = ");
+  Serial.print(channel);
+
+  Serial.print(" pitch = ");
+  Serial.print(pitch);
+
+  Serial.print(" velocity = ");
+  Serial.println(velocity);
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity) {
+  // Log when a note is released.
+  Serial.print("Note off: channel = ");
+  Serial.print(channel);
+
+  Serial.print(" pitch = ");
+  Serial.print(pitch);
+
+  Serial.print(" velocity = ");
+  Serial.println(velocity);
+}
+
+
+// core1's setup
+void setup1() {
+  //while (!Serial) {
+  // delay(100);   // wait for native usb
+  // }
+  Serial.printf("Core1 setup to run TinyUSB host with pio-usb\r\n");
+
+  // Check for CPU frequency, must be multiple of 120Mhz for bit-banging USB
+  uint32_t cpu_hz = clock_get_hz(clk_sys);
+  if ( cpu_hz != 120000000UL && cpu_hz != 240000000UL ) {
+    delay(2000);   // wait for native usb
+    Serial.printf("Error: CPU Clock = %u, PIO USB require CPU clock must be multiple of 120 Mhz\r\n", cpu_hz);
+    Serial.printf("Change your CPU Clock to either 120 or 240 Mhz in Menu->CPU Speed \r\n", cpu_hz);
+    while(1) delay(1);
+  }
+
+  pio_usb_configuration_t pio_cfg = PIO_USB_DEFAULT_CONFIG;
+  pio_cfg.pin_dp = PIN_USB_HOST_DP;
+ 
+ // Change the next line to #if 1 if the Pico-PIO-USB library version is 0.5.3 or older.
+ #if 0
+ #if defined(ARDUINO_RASPBERRY_PI_PICO_W)
+  /* Need to swap PIOs so PIO code from CYW43 PIO SPI driver will fit */
+  pio_cfg.pio_rx_num = 0;
+  pio_cfg.pio_tx_num = 1;
+ #endif /* ARDUINO_RASPBERRY_PI_PICO_W */
+ #endif
+ 
+  USBHost.configure_pio_usb(1, &pio_cfg);
+#ifdef PIN_5V_EN
+  pinMode(PIN_5V_EN, OUTPUT);
+  digitalWrite(PIN_5V_EN, PIN_5V_EN_STATE);
+#endif
+  // Optionally, configure the buffer sizes here
+  // The commented out code shows the default values
+  // tuh_midih_define_limits(64, 64, 16);
+
+  // run host stack on controller (rhport) 1
+  // Note: For rp2040 pico-pio-usb, calling USBHost.begin() on core1 will have most of the
+  // host bit-banging processing works done in core1 to free up core0 for other works
+  USBHost.begin(1);
+}
+
+// core1's loop
+void loop1()
+{
+  USBHost.task();
+}
+
+//--------------------------------------------------------------------+
+// TinyUSB Host callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device is mounted (configured)
+void tuh_mount_cb (uint8_t daddr)
+{
+  Serial.printf("Device attached, address = %d\r\n", daddr);
+
+  // Get Device Descriptor
+  tuh_descriptor_get_device(daddr, &desc_device, 18, print_device_descriptor, 0);
+}
+
+/// Invoked when device is unmounted (bus reset/unplugged)
+void tuh_umount_cb(uint8_t daddr)
+{
+  Serial.printf("Device removed, address = %d\r\n", daddr);
+}
+
+void print_device_descriptor(tuh_xfer_t* xfer)
+{
+  if ( XFER_RESULT_SUCCESS != xfer->result )
+  {
+    Serial.printf("Failed to get device descriptor\r\n");
+    return;
+  }
+
+  uint8_t const daddr = xfer->daddr;
+  // Get String descriptor using Sync API
+  uint16_t temp_buf[128];
+
+  Serial.printf("  iManufacturer       %u     "     , desc_device.iManufacturer);
+  if (XFER_RESULT_SUCCESS == tuh_descriptor_get_manufacturer_string_sync(daddr, LANGUAGE_ID, temp_buf, sizeof(temp_buf)) )
+  {
+    print_utf16(temp_buf, TU_ARRAY_SIZE(temp_buf));
+  }
+  Serial.printf("\r\n");
+
+  Serial.printf("  iProduct            %u     "     , desc_device.iProduct);
+  if (XFER_RESULT_SUCCESS == tuh_descriptor_get_product_string_sync(daddr, LANGUAGE_ID, temp_buf, sizeof(temp_buf)))
+  {
+    print_utf16(temp_buf, TU_ARRAY_SIZE(temp_buf));
+  }
+  Serial.printf("\r\n");
+
+  Serial.printf("  iSerialNumber       %u     "     , desc_device.iSerialNumber);
+  if (XFER_RESULT_SUCCESS == tuh_descriptor_get_serial_string_sync(daddr, LANGUAGE_ID, temp_buf, sizeof(temp_buf)))
+  {
+    print_utf16(temp_buf, TU_ARRAY_SIZE(temp_buf));
+  }
+  Serial.printf("\r\n");
+}
+
+//--------------------------------------------------------------------+
+// String Descriptor Helper
+//--------------------------------------------------------------------+
+
+static void _convert_utf16le_to_utf8(const uint16_t *utf16, size_t utf16_len, uint8_t *utf8, size_t utf8_len) {
+  // TODO: Check for runover.
+  (void)utf8_len;
+  // Get the UTF-16 length out of the data itself.
+
+  for (size_t i = 0; i < utf16_len; i++) {
+    uint16_t chr = utf16[i];
+    if (chr < 0x80) {
+      *utf8++ = chr & 0xff;
+    } else if (chr < 0x800) {
+      *utf8++ = (uint8_t)(0xC0 | (chr >> 6 & 0x1F));
+      *utf8++ = (uint8_t)(0x80 | (chr >> 0 & 0x3F));
+    } else {
+      // TODO: Verify surrogate.
+      *utf8++ = (uint8_t)(0xE0 | (chr >> 12 & 0x0F));
+      *utf8++ = (uint8_t)(0x80 | (chr >> 6 & 0x3F));
+      *utf8++ = (uint8_t)(0x80 | (chr >> 0 & 0x3F));
+    }
+    // TODO: Handle UTF-16 code points that take two entries.
+  }
+}
+
+// Count how many bytes a utf-16-le encoded string will take in utf-8.
+static int _count_utf8_bytes(const uint16_t *buf, size_t len) {
+  size_t total_bytes = 0;
+  for (size_t i = 0; i < len; i++) {
+    uint16_t chr = buf[i];
+    if (chr < 0x80) {
+      total_bytes += 1;
+    } else if (chr < 0x800) {
+      total_bytes += 2;
+    } else {
+      total_bytes += 3;
+    }
+    // TODO: Handle UTF-16 code points that take two entries.
+  }
+  return total_bytes;
+}
+
+static void print_utf16(uint16_t *temp_buf, size_t buf_len) {
+  size_t utf16_len = ((temp_buf[0] & 0xff) - 2) / sizeof(uint16_t);
+  size_t utf8_len = _count_utf8_bytes(temp_buf + 1, utf16_len);
+
+  _convert_utf16le_to_utf8(temp_buf + 1, utf16_len, (uint8_t *) temp_buf, sizeof(uint16_t) * buf_len);
+  ((uint8_t*) temp_buf)[utf8_len] = '\0';
+
+  Serial.printf((char*)temp_buf);
+}
+
+
+//--------------------------------------------------------------------+
+// TinyUSB Callbacks
+//--------------------------------------------------------------------+
+
+// Invoked when device with hid interface is mounted
+// Report descriptor is also available for use. tuh_hid_parse_report_descriptor()
+// can be used to parse common/simple enough descriptor.
+// Note: if report descriptor length > CFG_TUH_ENUMERATION_BUFSIZE, it will be skipped
+// therefore report_desc = NULL, desc_len = 0
+void tuh_midi_mount_cb(uint8_t dev_addr, uint8_t in_ep, uint8_t out_ep, uint8_t num_cables_rx, uint16_t num_cables_tx)
+{
+  Serial.printf("MIDI device address = %u, IN endpoint %u has %u cables, OUT endpoint %u has %u cables\r\n",
+      dev_addr, in_ep & 0xf, num_cables_rx, out_ep & 0xf, num_cables_tx);
+
+  if (midi_dev_addr == 0) {
+    // then no MIDI device is currently connected
+    midi_dev_addr = dev_addr;
+  }
+  else {
+    Serial.printf("A different USB MIDI Device is already connected.\r\nOnly one device at a time is supported in this program\r\nDevice is disabled\r\n");
+  }
+}
+
+// Invoked when device with hid interface is un-mounted
+void tuh_midi_umount_cb(uint8_t dev_addr, uint8_t instance)
+{
+  if (dev_addr == midi_dev_addr) {
+    midi_dev_addr = 0;
+    Serial.printf("MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+  }
+  else {
+    Serial.printf("Unused MIDI device address = %d, instance = %d is unmounted\r\n", dev_addr, instance);
+  }
+}
+
+void tuh_midi_rx_cb(uint8_t dev_addr, uint32_t num_packets)
+{
+  if (midi_dev_addr == dev_addr) {
+    if (num_packets != 0) {
+      uint8_t cable_num;
+      uint8_t buffer[48];
+      while (1) {
+        uint32_t bytes_read = tuh_midi_stream_read(dev_addr, &cable_num, buffer, sizeof(buffer));
+        if (bytes_read == 0)
+          return;
+        Serial.printf("MIDI RX Cable #%u:", cable_num);
+        for (uint32_t idx = 0; idx < bytes_read; idx++) {
+          Serial.printf("%02x ", buffer[idx]);
+          value[11]=buffer[2];
+        }
+        Serial.printf("\r\n");
+      }
+    }
+  }
+}
+
+void tuh_midi_tx_cb(uint8_t dev_addr)
+{
+    (void)dev_addr;
+}
+
